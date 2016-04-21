59183ae5554a6085efce2d9a8979717a6494c067
Kassim Ouattara <ouat.kassim@gmail.com>
2016-04-21 14:27:59 -0400

    basic layout

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+get '/home' do
+	erb :home
+end
+
+get '/jersey' do
+	erb :jersey
+end
+
+get '/shoes' do
+	erb :shoes
+end
+
+get '/contact' do
+	erb :contact
+end
+
+post '/send' do
+	puts "my params are " + params.inspect
+end
