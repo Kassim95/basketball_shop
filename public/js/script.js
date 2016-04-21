006439961e506920b00137ee2d1b7cfd13f3cb23
Kassim Ouattara <ouat.kassim@gmail.com>
2016-04-21 17:56:46 -0400

    creating pages

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,76 @@
-$(document).ready(function($) {
+var tabindex=1;
+$(document).ready(function(){
+  var windowWidthInit = $(".slider-wrap").width(),
+      slides = $(".slider .slide").length,
+      circle="",
+      move="";
+  $(".slider-wrap").hover(function(){
+    if (windowWidthInit>700) {
+      $(".arrow").fadeToggle();
+    }
+  });
+  $(".slide").css({"width": windowWidthInit});
+  for (var i=1;i<slides+1;i++) {
+    if (i===tabindex) {
+      circle = "<li><span class='circle'></circle></li>";
+    } else {
+      circle = "<li></li>";
+    }
+    $(circle).appendTo(".slider-wrap ul").bind("click", function(){
+      var oldtabindex = tabindex;
+      var windowWidth = $(".slider-wrap").width();
+      if ($(this).index()+1!=tabindex) {
+        tabindex=$(this).index()+1;
+        moveindex=tabindex-oldtabindex;
+        move="-="+windowWidth*(moveindex);
+        $(".slider").animate({left: move});
+      }
+      $(".circle").fadeOut().remove();
+      $("<span class='circle'></span>").appendTo(this).hide().fadeIn("fast");
+    });
+  }
+  $(".arrow").click(function(){
+    var windowWidth = $(".slider-wrap").width();
+    if ($(this).attr("id")==="right") {
+      if (tabindex===slides) {
+        tabindex=1
+          move="+="+windowWidth*(slides-1);
+      } else {
+        tabindex=tabindex+1;
+        move="-="+windowWidth;
+      }
+    } else {
+      if (tabindex===1){
+        tabindex=slides;
+        move="-="+windowWidth*(slides-1);
+      } else {
+        tabindex=tabindex-1;
+        move="+="+windowWidth;
+      }
+    }
+    $(".slider").animate({left: move});
+    $(".circle").fadeOut().remove();
+    $("<span class='circle'></span>").appendTo(".slider-wrap ul li:nth-child("+tabindex+")").hide().fadeIn();
+  });
+				var pushLeft = ($(".slider-wrap ul").width()/2);
+				$(".slider-wrap ul").css({"margin-left": "-"+pushLeft+"px"});
+  $('#modal').hide();
+  $('#content').hide();
+  $('.error2').hide();
 
-$('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+  $('.modal-link').click(function(e) {      
+    $('#content').show();
+    $('#modal').show(); 
+  });
+      
+  $('#modal').click(function(e) {     
+    $('#modal').hide();
+    $('#content').hide();
+});
+$(window).resize(function(){
+	var windowWidth = $(".slider-wrap").width();
+	$(".slide").css({"width": windowWidth});
 });
 
-});
+
+
